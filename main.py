c217262e960c4a1e445a635b782e6645ed5878ae
Gabriel Birnbaum <gbbinbum@gmail.com>
2022-08-13 09:32:15 -0700

    pretty sure energies and corrections are right now, but there are questions about LiO2

--- main.py
+++ main.py
@@ -20,13 +20,14 @@ if __name__ == '__main__':
     mpl.rcParams['figure.dpi'] = 300  # increase plot dpi
 
     # get compounds
+    # Corrections are taken from https://doi.org/10.1021/cm401720n
     xc = 'PBE'
-    #  TODO get right band gap with HSE06 values of the
-    #  Corrections are slightly adapted from A Facile Mechanism for Recharging
-    #  Li2O2 in Li−O2 Batteries paper. The adaptation is to be expected because DFT+U is implementation specific
-    Li2O = Compound('Li2O', 'mp-1960', xc, magmoms=[0.6, 0.6, 0.6], converged=True, setups={'O': ':p, -1.04,0'}, extension='cif')
-    Li2O2 = Compound('Li2O2', 'mp-841', xc, magmoms=[0.6, 0.6, 0.6, 0.6, 0.6, 0.6, 0.6, 0.6], setups={'O': ':p, -0.72,0'}, converged=True)
-    LiO2 = Compound('LiO2', 'mp-1018789', xc, magmoms=[0.6, 0.6, 0.6, 0.6, 0.6, 0.6],setups={'O': ':p, -0.24,0'}, converged=True)
+    Li2O = Compound('Li2O', 'mp-1960', xc, magmoms=[0.6, 0.6, 0.6], converged=True, setups={'O': ':p, -1.05,0'},
+                    extension='cif')
+    Li2O2 = Compound('Li2O2', 'mp-841', xc, magmoms=[0.6, 0.6, 0.6, 0.6, 0.6, 0.6, 0.6, 0.6],
+                     setups={'O': ':p, -0.75,0'}, converged=True)
+    LiO2 = Compound('LiO2', 'mp-1018789', xc, magmoms=[0.6, 0.6, 0.6, 0.6, 0.6, 0.6], setups={'O': ':p, -0.33,0'},
+                    converged=True)
     Li = Compound('Li', 'mp-1', xc, ecut=530, converged=True)
     O2 = Compound('O2', 'mp-12957', xc, magmoms=[0.6, 0.6, 0.6, 0.6], ecut=530, converged=True)
 
@@ -43,7 +44,8 @@ if __name__ == '__main__':
 
     print('epot_Li2O', epot_Li2O)
     print('epot_Li2O2', epot_Li2O2_cell)
-    print('epot_LiO2', epot_LiO2_cell)
+    print('epot_LiO2', epot_LiO2_cell)  # -to get voltage 2.88 you need 31.0206503975
+    print('epot_O2', epot_O2_cell)
 
     # get properties
     Li2O_gibbs = gibbs_energy(epot_Li2O, LI2O_ENTROPY, 2, 1)
@@ -51,7 +53,8 @@ if __name__ == '__main__':
     LiO2_gibbs = gibbs_energy(epot_LiO2, LIO2_ENTROPY, 1, 2)
     vol1 = get_eq_voltage(2 * Li2O_gibbs, Li2O2_gibbs, 2)
     vol2 = get_eq_voltage(2 * Li2O_gibbs, LiO2_gibbs, 3)
-    vol = (vol1+vol2)/2
+    vol = (vol1 + vol2) / 2
+    print(vol)
     x = 1.5
 
     get_mass_energy_density(vol, MOL_MASS_LI2O, x, watt_hour=True)
@@ -59,10 +62,9 @@ if __name__ == '__main__':
     get_specific_capacity(MOL_MASS_LI2O, x)
 
     # get electronic structure
-    for compound in [Li2O, Li2O2, LiO2]:
-        compound.get_bandgap()
-        compound.set_pdos()
-        compound.set_ldos()
-        compound.set_band_structure(emax=13)
-        compound.set_dos()
-
+    # for compound in [Li2O, Li2O2, LiO2]:
+    #     compound.get_bandgap()
+    #     compound.set_pdos()
+    #     compound.set_ldos()
+    #     compound.set_band_structure(emax=13)
+    #     compound.set_dos()
